1c7438b56dca54958522c365bdc618a01e1e109a
goldroid <70177400+goldroid@users.noreply.github.com>
2023-11-09 09:52:32 +0900

    컴퍼넌트완료

---
+++ src/views/HomeView.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Contact from "../components/Contact";
+import Intro from "../components/Intro";
+import Main from "../components/Main";
+import Port from "../components/Port";
+import Site from "../components/Site";
+import Skill from "../components/Skill";
+import Skip from "../components/Skip";
+
+
+const HomeView = () => {
+    return (
+    <>
+
+<Skip />
+        <Header />
+        <Main>
+            <Intro />
+            <Skill />
+            <Site />
+            <Port />
+            <Contact />
+        </Main>
+        <Footer />
+
+
+    </>
+    );
+};
+
+export default HomeView;
